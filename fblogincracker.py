15cd31e532a4f3db1edcd082d79a8a81d0ff1523
MohamedHacker <moh2012peace@hotmail.fr>
2018-02-07 16:36:18 +0100

    Update fblogincracker.py

--- fblogincracker.py
+++ fblogincracker.py
@@ -6,8 +6,8 @@ target = "https://www.facebook.com/login.php?login_attempt=1"
 response = b.open(target)
 
 b.select_form(nr = 0) # select form name from html file
-b.form['email'] = 'moh2012peace@hotmail.fr'
-b.form['pass'] = '#NoviceCoder2018#'
+b.form['email'] = 'email_here'
+b.form['pass'] = 'password_here'
 b.method = "POST"
 response = b.submit()
 
