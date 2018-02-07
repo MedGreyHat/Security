696ad3e6cd5d26f304af8cadd5a95a48f354ffed
MohamedHacker <moh2012peace@hotmail.fr>
2018-02-07 16:33:58 +0100

    Update fb_cracker.py

--- fb_cracker.py
+++ fb_cracker.py
@@ -1,8 +1,8 @@
 #_*_coding:utf_8_*_
 from oauth_tokens.providers.facebook import FacebookAuthRequest
-req = FacebookAuthRequest(username='Mohamed El Général', password='#NoviceCoder2018#')
+req = FacebookAuthRequest(username='username_here', password='password_here')
 response = req.authorized_request(url='https://facebook.com')
-response.content.count('Mohamed El Général')
+response.content.count('username_here')
 fileobj = open("HTML-OUTPUT.txt","wb")
 fileobj.write(response.content)
 fileobj.close()
