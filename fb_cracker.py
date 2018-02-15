5b0fab3614e10eb895e1b55f01187dcf4f78f92e
Mohamed_GH <moh2012peace@hotmail.fr>
2018-02-15 10:20:02 +0100

    Delete fb_cracker.py

--- fb_cracker.py
+++
@@ -1,8 +0,0 @@
-#_*_coding:utf_8_*_
-from oauth_tokens.providers.facebook import FacebookAuthRequest
-req = FacebookAuthRequest(username='username_here', password='password_here')
-response = req.authorized_request(url='https://facebook.com')
-response.content.count('username_here')
-fileobj = open("HTML-OUTPUT.txt","wb")
-fileobj.write(response.content)
-fileobj.close()
